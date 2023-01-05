fe3c3768b8e4c703f7f038af7b3ef90a7d4172d4
mathesh <matheshyogeswaran@outlook.com>
2023-01-05 16:00:35 +0530

    routing & app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Profile } from './pages/Profile';
+import { Search } from './pages/Search';
+import React from 'react';
+import {BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import {useState, createContext } from "react";
+export const AppContext = createContext();
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [userInput,setUserInput] = useState("");
+  return <div>
+    <AppContext.Provider value={{userInput,setUserInput}}>
+      <Router>
+          <Routes>
+              <Route path="/" element={<Search />} />
+              <Route path="/profile" element={<Profile />} />
+          </Routes>
+      </Router>
+      </AppContext.Provider>
+  </div>
 }
-
 export default App;
+
+
+
---
+++ src/pages/Search.js
@@ -0,0 +1,3 @@
+export const Search = ()=>{
+    return <h1>hi</h1>
+}
