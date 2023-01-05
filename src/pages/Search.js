07e9c7e21aea15b69108163106a0a5974abc138c
mathesh <matheshyogeswaran@outlook.com>
2023-01-05 16:11:00 +0530

    Search.js setup

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,3 +1,23 @@
+import { useContext } from "react"
+import { AppContext } from "../App"
+import "../App.css";
+import { useNavigate } from 'react-router-dom';
 export const Search = ()=>{
-    return <h1>hi</h1>
+    const navigate = useNavigate();
+    const {userInput,setUserInput} = useContext(AppContext);
+    async function LogUser (){
+        navigate('/profile');
+    };
+
+    return (
+    <form onSubmit={LogUser}><br></br>
+        <input 
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+            type="text" 
+            placeholder="User Name"/>
+            <br></br>
+        <input type="submit" value="Register"/>
+      </form>
+    );
 }
